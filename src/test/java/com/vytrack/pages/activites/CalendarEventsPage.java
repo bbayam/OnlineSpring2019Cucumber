e3abbd5c83daffb5f88eeabe92f9189f8c025a96
bbayam <bsrbym@hotmail.com>
2019-08-16 18:19:04 -0500

    change for branches

--- src/test/java/com/vytrack/pages/activites/CalendarEventsPage.java
+++ src/test/java/com/vytrack/pages/activites/CalendarEventsPage.java
@@ -219,4 +219,9 @@ public class CalendarEventsPage extends BasePage {
     public String getEndTime() {
         return endTime.getAttribute("value");
     }
+
+    public void nothing(){
+
+    }
 }
+
